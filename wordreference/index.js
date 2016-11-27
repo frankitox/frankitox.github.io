84552dd7ee7db58d2ee24b9de05a3eb06cf1bc67
franquito <franconbiasin@gmail.com>
2016-11-27 19:43:17 -0300

    Aux commit

--- wordreference/index.js
+++ wordreference/index.js
@@ -1,41 +1,3 @@
-function splitMultiple(chars, string) {
-  if (!_.isString(string) || !_.isArray(chars)) {
-    throw new Error('Bad parameters for splitMultiple');
-  }
-
-  if (chars.length === 0) return [string];
-
-  const firstSplit = _.split(_.head(chars), string);
-  function fun(arr, c) {
-    return _.flatMap(_.split(c), arr);
-  }
-  const reduced = _.reduce(
-    (words, ch) => _.flatMap(_.split(ch), words),
-    firstSplit,
-    _.tail(chars)
-  );
-  return _.filter((elem) => !_.isEmpty(elem), reduced);
-}
-
-function deCons(array) {
-  console.assert(_.isArray(array),
-                 'Bad param for deCons');
-  console.assert(array.length !== 0,
-                 'The array must be non-empty');
-  return [_.head(array), _.tail(array)];
-}
-
-_.mixin({
-  'splitMultiple': splitMultiple,
-  'deCons': deCons,
-});
-
-// Usage example:
-// _.splitMultiple([" ", "\n", "\t"], "Hello everyone!\n\tMy name's Franco.")
-// => ["Hello", "everyone!", "My", "name's", "Franco."]
-
-var q = _.flatMap(["Hi\nman", "Other"], _.split('\n'));
-
 // String -> Audio | String
 function audioFromWord(word) {
   if (!_.isString(word)) {
@@ -80,9 +42,105 @@ function playWords(words) {
     });
 }
 
-const Words = {};
+const Words = (() => {
+  return {
+    // TODO: Move this to private.
+    storedWords: {},
 
-const Editor = (() => {
+    getWord(word) {
+      console.assert(
+        storedWords[word] !== undefined,
+        'The word\'s not stored');
+
+      return storedWords[word].promise();
+    },
+
+    // string -> void
+    storeWord(word) {
+      if (this.storedWords[word] !== undefined)
+        return;
+
+      const downloadDeferred = $.Deferred();
+      $.get(`http://www.wordreference.com/definition/${word}`)
+       .done((html) => {
+         const noImgHtml = _.replace(/<img[^\/>]*/gi, '', html);
+         const sourceRegExp = /<source src='([^>]*)'>/gi;
+         const sourcesTags = noImgHtml.match(sourceRegExp);
+         if (_.isNull(sourcesTags)) {
+           console.warn(`No sources found for ${word}`);
+           downloadDeferred.reject(`No sources found for ${word}`);
+         }
+         const sources = _.map((sourceTag) => sourceTag.match(/src='([^']*)'/)[1],
+           sourcesTags);
+         const ukRelativeUrl = _.find((source) => _.contains('uk/general', source), sources);
+         if (ukRelativeUrl === undefined) {
+           return `No uk/general audio found`;
+           console.warn(`No uk/general audio found`);
+           downloadDeferred.reject(`No uk/general audio found`);
+         }
+         const audio = new Audio(
+           `http://www.wordreference.com${ukRelativeUrl}`);
+         downloadDeferred.resolve(audio);
+       });
+      this.storedWords[word] = downloadDeferred;
+      return downloadDeferred.promise();
+    },
+
+    // string[] -> void
+    storeWords(words) {
+      _.forEach((word) => {
+        this.storeWord(word);
+      }, words);
+    },
+  };
+})();
+
+const Talker = (() => {
+  const publicObject = {};
+
+  let currentSpokenWord; /* : $.Deferred */
+
+  /* private */ function recursiveSay(words, everyWordSaid) {
+    if (words.length === 0) {
+      everyWordSaid.resolve();
+    } else {
+      const word = Words.getWord(_.head(words));
+      const wordsLeft = _.tail(words);
+      word.done((audio) => {
+        $(audio).on('ended', () => {
+          
+        });
+        audio.play();
+      }).fail(() => {
+        
+      });
+    }
+  }
+
+  publicObject.say = function say(words) {
+    const alreadyTalking = currentSpokenWord !== undefined;
+    if (alreadyTalking) {
+      return undefined;
+    }
+
+    const everyWordSaid = $.Deferred();
+    if (words.length === 0) {
+      everyWordSaid.resolve();
+    } else {
+      currentSpokenWord = $.Deferred();
+      recursiveSay(words, everyWordSaid);
+    }
+    return everyWordSaid.promise();
+  };
+
+  publicObject.stop = function stop() {
+    
+  };
+
+  return publicObject;
+})();
+
+const App = (() => {
   let sayButton;
   let iframe;
 
@@ -103,43 +161,43 @@ const Editor = (() => {
   }
 
   /* private */ function loop() {
-    setInterval(this.checkText.bind(this), 250);
+    setInterval(checkText.bind(this), 500);
   }
 
   /* private */ function checkText() {
+    const words = getWords.bind(this)();
+    Words.storeWords(words);
+  }
+
+  /* private */ function getWords() {
     const text =
       $(iframe.get(0).contentWindow.document)
         .find('body').text();
-
-    _.words(_.lowerCase(text));
+    return _.words(_.lowerCase(text));
   }
 
   return {
     start() {
-      this.initialization();
-      this.loop();
+      initialization.bind(this)();
+      loop.bind(this)();
+    },
+    talk() {
+      const words = getWords.bind(this);
+    },
+
+    currentPromise: undefined,
+    currentTalkedWord(words) {
+      if (words.length === 0) {
+        // TODO: Reset button.
+        return;
+      }
+      const promisedWord =
+        Words.getWord(_.head(words));
     },
   };
 })();
 
 $(document).ready(() => {
   const iframe = $('iframe').get(0);
-  $(iframe).on('load', Editor.start.bind(Editor));
+  $(iframe).on('load', App.start.bind(App));
 });
-
-// $(document).ready(() => {
-//   $('button#say').on('click', (e) => {
-//     $('#messages').empty();
-//     const text = $('textarea#text-to-say').val();
-//     const words = _.words(text);
-//     playWords(words);
-//   });
-// 
-//   const iframe = $('iframe').get(0);
-//   $(iframe).on('load', () => {
-//     iframe.contentDocument.designMode = 'on';
-//     $(iframe.contentWindow.document).on('keyup', (e) => {
-//       console.log($(e.target).text());
-//     });
-//   });
-// });
